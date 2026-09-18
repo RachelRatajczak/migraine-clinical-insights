bb79c85ce2ce8040e7a285d76be7ffaa3813aa56
ConnorPekovic <rachelratajczak@outlook.com>
2026-09-17 20:45:45 -0500

    add queries

--- scripts/03_exploratory_queries.py
+++ scripts/03_exploratory_queries.py
@@ -29,12 +29,54 @@ query_type_summary = """
 """
 df_type_summary = pd.read_sql(query_type_summary, conn)
 
-print("--- Migraine Type & Intensity Summary ---")
+print("\n--- Migraine Type & Intensity Summary ---")
 print(df_type_summary.head())
 
 # Export this summary table for Tableau later
 df_type_summary.to_csv("data/tableau_migraine_type_summary.csv", index=False)
 
+# =============================================================================
+# 2. QUERY 2: AGE-SEVERITY RELATIONSHIP
+# =============================================================================
+
+query_age_intensity = """
+    SELECT 
+    CASE 
+        WHEN age BETWEEN 18 AND 29 THEN '18-29'
+        WHEN age BETWEEN 30 AND 44 THEN '30-44'
+        WHEN age BETWEEN 45 AND 59 THEN '45-59'
+        ELSE '60+'
+    END as age_group,
+    ROUND(AVG(intensity), 2) as avg_intensity
+    FROM clinical_encounters
+    WHERE age IS NOT NULL
+    GROUP BY age_group
+    ORDER BY age_group;
+"""
+
+df__age_intensity = pd.read_sql(query_age_intensity, conn)
+
+print("\n--- Age & Intensity ---")
+print(df__age_intensity.head())
+
+# Export this summary table for Tableau later
+df__age_intensity.to_csv("data/tableau_age_intensity.csv", index=False)
+
+# =============================================================================
+# 2. QUERY 3: MULTI-SYMPTOM IN SEVERE CASES
+# =============================================================================
+
+query_multi_symptoms = """
+
+"""
+
+# =============================================================================
+# 2. QUERY 4: DURATION BY MIGRAINE TYPE
+# =============================================================================
+
+query_duration = """
+
+"""
 
 # =============================================================================
 # 3. CLOSE DATABASE CONNECTION
